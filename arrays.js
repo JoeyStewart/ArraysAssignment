5f98f9b33f6863ce445318b7d1a6df7279b1d143
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-01 18:56:11 -0400

    Add Array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var people = ["Fred", "Jimbo", "Cornelius", "Pepperjack"];
 var numbers = [12, 76, 52, 33];
 var goodRadioSongs = [false, true, false, false];
+
+people.push()
+console.log(people)
+
+numbers.shift()
+console.log(numbers)
+
+goodRadioSongs.pop()
+console.log(goodRadioSongs)
