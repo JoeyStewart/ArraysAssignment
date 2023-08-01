3fae6ad6721815208d5af01680f4f4d0fcba54cf
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-01 19:01:38 -0400

    Explain Index

--- arrays.js
+++ arrays.js
@@ -10,3 +10,8 @@ console.log(numbers)
 
 goodRadioSongs.pop()
 console.log(goodRadioSongs)
+
+// The index position is the position of the values' position within a variable.
+// Index positions start at 0 rather than 1. 
+// An example would be identifing 76 in the numbers variable as index position 1,
+// since 12 would be 0 intead of itself as 1 under normal contexts outside of Javascript.
